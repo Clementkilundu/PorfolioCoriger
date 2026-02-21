f1eb51df19e3745bd6aa62508324463c66ffddbb
Clement Kilundu <kilunduclement@gmail.com>
2026-02-21 17:32:58 +0100

    Ajout des fichiers du portfolio avec bootstrap

---
+++ Models/Project.cs
@@ -0,0 +1,14 @@
+﻿namespace MonPorfolio.Models
+{
+    public class Project
+    {
+        public required string Title { get; set; }
+        public required string ShortDescription { get; set; }
+        public required string Description { get; set; }
+        public required string TechStack { get; set; }
+        public required string Color { get; set; }
+        public string? ImageUrl { get; set; }
+
+        public string? demo { get; set; }
+    }
+}
